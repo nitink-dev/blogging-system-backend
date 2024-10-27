bd14d6fde7c6485c40edc9634e7b53af7cd469a9
nitinkumar <nitink.dev@gmail.com>
2024-10-27 13:17:03 +0530

    init commit

---
+++ src/main/java/com/bogging/system/Blogging/System/BloggingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bogging.system.Blogging.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bogging/system/Blogging/System/BloggingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogging.system.Blogging.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
