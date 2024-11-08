1aaef77f4f7012e9b5ee2418edd29d9669a3156e
KDAppsDec22 <kaushaldubey71@gmail.com>
2024-11-08 23:43:10 +0530

    changes in user controller

--- src/main/java/com/bogging/system/Blogging/System/Controller/UserController.java
+++ src/main/java/com/bogging/system/Blogging/System/Controller/UserController.java
@@ -31,10 +31,10 @@ public class UserController {
         return userService.createUser(user);
     }
 
-    /*@PutMapping("/{id}")
+    @PutMapping("/{id}")
     public User updateUser(@PathVariable Long id, @RequestBody User user) {
         return userService.updateUser(id, user);
-    }*/
+    }
 
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable Long id) {
